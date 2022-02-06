37cbdfa8458864516263fafdfc58e0ab6874bc32
jdroutine <jd.routine@gmail.com>
2022-02-06 15:02:27 +0100

    homepage url path, view added

--- src/warehouse/views.py
+++ src/warehouse/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def home(request):
+    title = 'Witaja na naszej stronie!'
+    form = 'jakiś tam napis'
+    context = {
+        "title": title,
+
+    }
+    return render(request, "home.html", context)
