b33a0992adccd0f14d009be71b418665f403570b
jdroutine <jd.routine@gmail.com>
2022-02-06 13:57:41 +0100

    ItemCreateForm added

--- src/warehouse/admin.py
+++ src/warehouse/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from .forms import ItemCreateForm
 
 # Register your models here.
+
 from .models import Item
 
-admin.site.register(Item)
+
+class ItemCreateAdmin(admin.ModelAdmin):
+    list_display = ['category', 'item_name', 'quantity']
+    form = ItemCreateForm
+    list_filter = ['category']
+    search_fields = ['category', 'item_name']
+
+
+admin.site.register(Item, ItemCreateAdmin)
