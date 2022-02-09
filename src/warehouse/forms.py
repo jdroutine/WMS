40c42134705352ec2fc544d8eb3ed1c928019274
jdroutine <jd.routine@gmail.com>
2022-02-10 00:51:46 +0100

    forms validators added

--- src/warehouse/forms.py
+++ src/warehouse/forms.py
@@ -7,6 +7,25 @@ class ItemCreateForm(forms.ModelForm):
         model = Item
         fields = ['item_name', 'category', 'brand', 'quantity']
 
+    def clean_category(self):
+        category = self.cleaned_data.get('category')
+        if not category:
+            raise forms.ValidationError('This field is required')
+        return category
+
+    def clean_item_name(self):
+        item_name = self.cleaned_data.get('item_name')
+        if not item_name:
+            raise forms.ValidationError('This field is required')
+
+        for instance in Item.objects.all():
+            if instance.item_name == item_name:
+                raise forms.ValidationError(item_name + ' is aleready created')
+        return item_name
+
+
+5
+
 
 class ItemSearchForm(forms.ModelForm):
     class Meta:
